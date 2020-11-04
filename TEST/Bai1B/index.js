2cc6713e8e5e6cbcc61c71a6ec4a9491c46154b6
minhkhue3214 <71182602+minhkhue3214@users.noreply.github.com>
2020-11-04 22:09:37 +0700

    Add files via upload

--- TEST/Bai1B/index.js
+++ TEST/Bai1B/index.js
@@ -1,15 +1,21 @@
-// function checkString(str1, str2) {
-//     let finalStr = ``;
-//     let totalLength = 0;
-//     if (str1.length > str2.length) {
-//         totalLength = str1.length;
-//     } else {
-//         totalLength = str2.length;
-//     }
-//     for (let i = 0; i < totalLength; i++) {
-//         let char1 = str1[i];
-//         let char2 = str2[i];
-//         if (str1[i] == undefined) {
-//             char1 = ``;
-//         } else if (str2[i] == undefined) {
-//             char2 = ``;
+function mergeString(string1, string2) {
+    let finalString = ``;
+    let totalLength = 0;
+    if (string1.length > string2.length) {
+        totalLength = str1.length;
+    } else {
+        totalLength = string2.length;
+    }
+    for (let i = 0; i < totalLength; i++) {
+        let char1 = string1[i];
+        let char2 = string2[i];
+        if (string1[i] == undefined) {
+            char1 = ``;
+        } else if (string2[i] == undefined) {
+            char2 = ``;
+        }
+        finalString += char1 + char2;
+    }``
+    return finalString;
+}
+// console.log(mergeString(`abc`, `123`));
